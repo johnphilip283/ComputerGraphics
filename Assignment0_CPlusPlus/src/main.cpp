10a0550bbb4b9fcef09dab944fc894730f7ca771
John Philip <johnphilip283@gmail.com>
2020-01-21 16:23:44 -0500

    Finished darkennand got pictures to display.

--- Assignment0_CPlusPlus/src/main.cpp
+++ Assignment0_CPlusPlus/src/main.cpp
@@ -1,11 +1,31 @@
 // Include our custom library
 #include "../include/PPM.h"
+#include <iostream>
+#include <fstream>
+#include "ppm.cpp"
 
-int main(){
+int main() {
 
-    PPM myPPM("./textures/test1.ppm");
-    // myPPM.darken();
-    // myPPM.savePPM("./textures/test1_darken.ppm");
+    PPM myPPM("../textures/test.ppm");
+    myPPM.darken();
+    myPPM.savePPM("../textures/test_darken.ppm");
 
+    return 0;
+}
+
+int main(int argc, char** argv) {
+
+    if (argc != 2) {
+        std::cout << "Oops, exactly 2 arguments needed.\n";
+        std::cout << "file_path\n";
+        return 0;
+    }
+    
+    std::string fileName = argv[1];
+    std::string outputFile = "./textures/" + fileName + "_darken.ppm";
+
+    PPM myPPM(argv[1]);
+    myPPM.darken();
+    myPPM.savePPM(outputFile);
     return 0;
 }
--- Assignment0_CPlusPlus/src/ppm.cpp
+++ Assignment0_CPlusPlus/src/ppm.cpp
@@ -9,64 +9,55 @@ PPM::PPM(string fileName) {
     ifstream inFile;
 
     inFile.open(fileName);
-    
-    int index = 0;
-    string a;
-    while (index != 5) {
-        inFile >> a;
-        cout << a;
-        break;
-        index++;
+
+    if (inFile.is_open()) {
+        
+        string line;
+        int index = 0;
+        
+        while (getline(inFile, line) && index != 1) {
+            index++;
+        }
+
+        inFile >> m_width;
+        inFile >> m_height;
+
+        unsigned int size = (m_width * m_height * 3) + 1;
+        m_PixelData = new unsigned char[size];
+
+        for (unsigned int i = 0; i < size; i++) {
+            int number;
+            inFile >> number;
+            m_PixelData[i] = number;
+        }
+
+        inFile.close();
     }
 
-        
-        // if (index == 2) {
-        //     char* line;   // A string object.
-        //     getline(inFile, line);
-        //     char str[] = line;
-        //     char delim = " ";
-
-        //     char* token = strtok(str, " ");
-        //     string measurements[] = new string[2];
-
-        //     while (token != NULL) {
-
-        //     }
-        // }
-        
-        
-
-        // if (index == 2) {
-
-        //     char* tok = strtok(line, " ");
-        //     char* first = tok;
-        //     char* second;
-
-        //     while (tok != NULL) {
-        //         second = strtok(NULL, " ");
-        //     }
-
-        //     m_PixelData = new unsigned char[];
-        // }
-
-        
-        // while(getline(inFile,line)){
-        //     cout << line << endl;
-        // }
-        // index++;
-    // }
-    // TODO:    Load and parse a ppm to get its pixel
-    //          data stored properly.
 }
 
 // Destructor clears any memory that has been allocated
 PPM::~PPM() {
+    delete[] m_PixelData;
 }
 
 // Saves a PPM Image to a new file.
 void PPM::savePPM(string outputFileName) {
-    // TODO: Save a PPM image to disk
+
+    // opens a file to output 
+    ofstream outFile;
+    outFile.open(outputFileName);
+    outFile << "P3" << endl;
+    outFile << "# " + outputFileName << endl;
+    outFile << m_width << " " << m_height << endl;
+
+    unsigned int size = (m_width * m_height * 3) + 1;
+    for (unsigned int i = 0; i < size; i++) {
+        outFile << (int) m_PixelData[i] << endl;
+    }
+    outFile.close();
 }
+
 
 // Darken subtracts 50 from each of the red, green
 // and blue color components of all of the pixels
@@ -77,8 +68,13 @@ void PPM::darken() {
     for (int i = 0; i < m_width; i++) {
         for (int j = 0; j < m_height; j++) {
             int index = (m_width * j * 3) + (i * 3);
-            if (m_PixelData[index] - 50 < 0 || m_PixelData[index + 1] - 50 < 0 || m_PixelData[index + 2] - 50 < 0) {
-                setPixel(i, j, m_PixelData[index] - 50, m_PixelData[index + 1] - 50, m_PixelData[index + 2] - 50);
+
+            int r = (int) m_PixelData[index];
+            int g = (int) m_PixelData[index + 1];
+            int b = (int) m_PixelData[index + 2];
+
+            if (r > 50 && g > 50 && b > 50) {
+                setPixel(i, j, r - 50, g - 50, b - 50);
             }
         }
     }
@@ -87,7 +83,7 @@ void PPM::darken() {
 // Sets a pixel to a specific R,G,B value 
 void PPM::setPixel(int x, int y, int R, int G, int B) {
     int index = (m_width * y * 3) + (x * 3);
-    m_PixelData[index] = R;
-    m_PixelData[index + 1] = G;
-    m_PixelData[index + 2] =  B;
+    m_PixelData[index] = (char) R;
+    m_PixelData[index + 1] = (char) G;
+    m_PixelData[index + 2] =  (char) B;
 }
