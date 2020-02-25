10f03e3aa0be6cbb0df307d2916147a1f2cf5f7c
John Philip <johnphilip283@gmail.com>
2020-02-25 18:00:48 -0500

    Updating lab 5

--- Lab5_Part2/BasicWidget.cpp
+++ Lab5_Part2/BasicWidget.cpp
@@ -2,18 +2,13 @@
 
 //////////////////////////////////////////////////////////////////////
 // Publics
-#if USE_QT_OPENGL
-  BasicWidget::BasicWidget(QWidget* parent) : QOpenGLWidget(parent), vbo_(QOpenGLBuffer::VertexBuffer), cbo_(QOpenGLBuffer::VertexBuffer), ibo_(QOpenGLBuffer::IndexBuffer)
-#else
-  BasicWidget::BasicWidget(QWidget* parent) : QOpenGLWidget(parent)
-#endif
+BasicWidget::BasicWidget(QWidget* parent) : QOpenGLWidget(parent), vbo_(QOpenGLBuffer::VertexBuffer), cbo_(QOpenGLBuffer::VertexBuffer), ibo_(QOpenGLBuffer::IndexBuffer)
 {
   setFocusPolicy(Qt::StrongFocus);
 }
 
 BasicWidget::~BasicWidget()
 {
-#if USE_QT_OPENGL
   vbo_.release();
   vbo_.destroy();
   ibo_.release();
@@ -22,12 +17,6 @@ BasicWidget::~BasicWidget()
   cbo_.destroy();
   vao_.release();
   vao_.destroy();
-#else
-  glDeleteProgram(shaderID_);
-  glDeleteBuffers(1, &vboID_);
-  glDeleteBuffers(1, &cboID_);
-  glDeleteBuffers(1, &iboID_);
-#endif
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -60,7 +49,6 @@ QString BasicWidget::fragmentShaderString() const
   return str;
 }
 
-#if USE_QT_OPENGL
 void BasicWidget::createShader()
 {
   QOpenGLShader vert(QOpenGLShader::Vertex);
@@ -80,63 +68,6 @@ void BasicWidget::createShader()
 	qDebug() << shaderProgram_.log();
   }
 }
-#else
-void BasicWidget::createShader()
-{
-  unsigned int prog = glCreateProgram();
-  unsigned int vert = compileShader(GL_VERTEX_SHADER, vertexShaderString());
-  unsigned int frag = compileShader(GL_FRAGMENT_SHADER, fragmentShaderString());
-  glAttachShader(prog, vert);
-  glAttachShader(prog, frag);
-  glLinkProgram(prog);
-  glValidateProgram(prog);
-
-  // The shaders are build and ready to use.  We can now detach the individual shaders and destroy them
-  glDetachShader(prog, vert);
-  glDetachShader(prog, frag);
-  glDeleteShader(vert);
-  glDeleteShader(frag);
-
-  // Assign here in case there are failures we need to bail on - This has not yet been implemented, though.
-  shaderID_ = prog;
-}
-
-unsigned int BasicWidget::compileShader(unsigned int type, const QString& shaderSrc)
-{
-  unsigned int id = glCreateShader(type);
-  // Make sure we can create the shader
-  if(id == 0) {
-	return 0;
-  }
-  QByteArray ba = shaderSrc.toLocal8Bit();
-  const char* src = ba.data();
-  // Set the source
-  glShaderSource(id, 1, &src, nullptr);
-  // Compile
-  glCompileShader(id);
-  // Retrieve the result of our compilation
-  int result;
-  glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-  if(result == GL_FALSE){
-	int length;
-	glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-	char* errorMessages = new char[length]; // Could also use alloca here.
-	glGetShaderInfoLog(id, length, &length, errorMessages);
-	if(type == GL_VERTEX_SHADER){
-	  qDebug() << "ERROR: GL_VERTEX_SHADER compilation failed!\n" << errorMessages << "\n";
-	}else if(type == GL_FRAGMENT_SHADER){
-	  qDebug() << "ERROR: GL_FRAGMENT_SHADER compilation failed!\n" << errorMessages << "\n";
-	}
-	// Reclaim our memory
-	delete[] errorMessages;
-	// Delete our broken shader
-	glDeleteShader(id);
-	return 0;
-  }
-
-  return id;
-}
-#endif
 ///////////////////////////////////////////////////////////////////////
 // Protected
 void BasicWidget::keyReleaseEvent(QKeyEvent* keyEvent)
@@ -181,7 +112,7 @@ void BasicWidget::initializeGL()
 	-0.8f,  0.8f, 0.0f,  // Top vertex position
     0.8f, 0.8f, 0.0f
   };
-  // Define our vert colors
+  // // Define our vert colors
   static const GLfloat colors[16] =
   {
       1.0f, 0.0f, 0.0f, 1.0f, // red
@@ -194,8 +125,9 @@ void BasicWidget::initializeGL()
   {
       0, 1, 2, 2, 1, 3
   };
+
   // Set up our buffers and our vao
-#if USE_QT_OPENGL
+
   // Temporary bind of our shader.
   shaderProgram_.bind();
   // Create and prepare a vbo
@@ -230,26 +162,7 @@ void BasicWidget::initializeGL()
   // Releae the vao THEN the vbo
   vao_.release();
   shaderProgram_.release();
-#else
-  vao_.create();
-  vao_.bind();
 
-  glGenBuffers(1, &vboID_);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboID_);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 12 * sizeof(GL_FLOAT), verts, GL_STATIC_DRAW);
-
-  glGenBuffers(1, &cboID_);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cboID_);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 16 * sizeof(GL_FLOAT), colors, GL_STATIC_DRAW);
-
-  glGenBuffers(1, &iboID_);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboID_);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(GL_INT), idx, GL_STATIC_DRAW);
-
-  vao_.release();
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-#endif
   glViewport(0, 0, width(), height());
 }
 
@@ -260,46 +173,17 @@ void BasicWidget::resizeGL(int w, int h)
 
 void BasicWidget::paintGL()
 {
-  glDisable(GL_DEPTH_TEST);
+  // glDisable(GL_DEPTH_TEST);
   glDisable(GL_CULL_FACE);
 
   glClearColor(0.f, 0.f, 0.f, 1.f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-#if USE_QT_OPENGL
   shaderProgram_.bind();
   vao_.bind();
   // TODO: Change number of indices drawn
-  glDrawElements(GL_TRIANGLES, indicesToDraw, GL_UNSIGNED_INT, 0);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
   // ENDTODO
   vao_.release();
   shaderProgram_.release();
-#else
-  glUseProgram(shaderID_);
-  glEnableVertexAttribArray(0);
-  glBindBuffer(GL_ARRAY_BUFFER, vboID_);
-  glVertexAttribPointer(0,        // Attribute 0 matches our layout for vertex positions
-      3,        // Size
-      GL_FLOAT, // Type
-      GL_FALSE, // Not normalized
-      0,        // Stride - no interleaving
-      (void*)0  // nullptr
-  );
-  glEnableVertexAttribArray(1);
-  glBindBuffer(GL_ARRAY_BUFFER, cboID_);
-  glVertexAttribPointer(1,        // Attribute 0 matches our layout for vertex positions
-      4,        // Size
-      GL_FLOAT, // Type
-      GL_FALSE, // Not normalized
-      0,        // Stride - no interleaving
-      (void*)0  // nullptr
-  );
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboID_);
-  // Render
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-
-  // Unbind everything
-  glDisableVertexAttribArray(0);
-  glUseProgram(NULL);
-#endif
 }
